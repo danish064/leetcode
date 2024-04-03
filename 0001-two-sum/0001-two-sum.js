e27ca85c2ff95f0e8b70c3e3c8192aae59f750d1
Danish Ahmed <33063661+danish064@users.noreply.github.com>
2024-04-03 05:40:54 +0500

    Time: 123 ms (19.8%), Space: 49 MB (96.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,7 +5,7 @@
  */
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
+        for (let j = i+1; j < nums.length; j++) {
             if (i != j) {
                 if (nums[i] + nums[j] == target) {
                     return new Array(i, j)
