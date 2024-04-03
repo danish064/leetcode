a414cfbd09baf78054231759680ca013b20938e1
Danish Ahmed <33063661+danish064@users.noreply.github.com>
2024-04-03 05:42:40 +0500

    Time: 100 ms (38.31%), Space: 49 MB (96.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,11 +5,9 @@
  */
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i+1; j < nums.length; j++) {
-            if (i != j) {
-                if (nums[i] + nums[j] == target) {
-                    return new Array(i, j)
-                }
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return new Array(i, j)
             }
         }
     }
