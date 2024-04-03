85f2a1e6e205762e02da6cba86fddaab77d2e5a7
Danish Ahmed <33063661+danish064@users.noreply.github.com>
2024-04-04 01:53:18 +0500

    Time: 101 ms (83.08%), Space: 54.7 MB (93.65%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -12,28 +12,26 @@
  */
 var addTwoNumbers = function (l1, l2) {
     let resultList = new ListNode();
-    let resultPtr = resultList;
-    let ptr1 = l1;
-    let ptr2 = l2;
-    let carry = 0;
-    while (ptr1 != null || ptr2 != null || carry > 0) {
-        let num1 = ptr1 ? ptr1.val : 0;
-        let num2 = ptr2 ? ptr2.val : 0;
-        let sum = num1 + num2;
-        if (carry > 0) {
-            sum = sum + 1;
-            carry = 0;
-        }
-        if (sum > 9) {
-            sum = sum - 10;
-            carry = 1;
-        }
-        const resultNode = new ListNode(sum);
-        resultPtr.next = resultNode;
-        resultPtr = resultNode;
-
-        ptr1 = ptr1?.next ? ptr1.next : null;
-        ptr2 = ptr2?.next ? ptr2.next : null;
+  let resultPtr = resultList;
+  let carry = 0;
+  while (l1 != null || l2 != null || carry > 0) {
+    let num1 = l1 ? l1.val : 0;
+    let num2 = l2 ? l2.val : 0;
+    let sum = num1 + num2;
+    if (carry > 0) {
+      sum = sum + 1;
+      carry = 0;
     }
-    return resultList.next;
+    if (sum > 9) {
+      sum = sum - 10;
+      carry = 1;
+    }
+    const resultNode = new ListNode(sum);
+    resultPtr.next = resultNode;
+    resultPtr = resultNode;
+
+    l1 = l1?.next ? l1.next : null;
+    l2 = l2?.next ? l2.next : null;
+  }
+  return resultList.next;
 };
