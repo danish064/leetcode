e606ead759860706bc06c2792933a203dc5b6f1b
Danish Ahmed <33063661+danish064@users.noreply.github.com>
2025-02-14 17:01:45 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,11 +2,11 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function () {
-    if (this.length == 0) {
-        return -1;
-    }
+    // if (this.length == 0) {
+    //     return -1;
+    // }
 
-    return this[this.length - 1];
+    return this[this.length - 1] ?? -1;
 };
 
 /**
